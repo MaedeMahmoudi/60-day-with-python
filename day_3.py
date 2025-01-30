09f81b74075418679edfc68f36140eb5f7104f03
Maedeh Mahmoudi <maedeemahmoudi5@gmail.com>
2025-01-30 23:39:34 +0330

    knowing about calculate operators

---
+++ day_3.py
@@ -0,0 +1,38 @@
+
+
+#number & math day3
+
+print("i will now count my chicken : ")
+print(" hens " , 25+30/6 )
+print(" roosters " , 100 - 25 * 3 % 4 )
+
+print(" now i will count the eggs : ")
+print( 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6 )
+
+print("is it true that 3 + 2 < 5-7 ?")
+print( 3 + 2 < 5 - 7 )
+
+print("what is 3 + 2 ?", 3 + 2 )
+print("what is 5-7 ? " , 5 - 7 )
+
+print("oh , that's why , it's false ")
+
+print("is it greater‌ ? " , 5 > -2 )
+print("is it greater‌ or equal ? " , 5 >= -2 )
+print("is it less or equal ? " , 5 <= -2 )
+  
+  
+  
+#i will now count my chicken : 
+#hens  30.0
+#roosters  97
+#now i will count the eggs : 
+#6.75
+#is it true that 3 + 2 < 5-7 ?
+#False
+#what is 3 + 2 ? 5
+#what is 5-7 ?  -2
+#oh , that's why , it's false 
+#is it greater‌ ?  True
+#is it greater‌ or equal ?  True
+#is it less or equal ?  False
