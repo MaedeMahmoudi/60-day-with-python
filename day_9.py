b8898a2f6c2f8ab0367f954c272c8b407e52c713
Maedeh Mahmoudi <maedeemahmoudi5@gmail.com>
2025-02-06 00:37:11 +0330

    printing , printing , printing :D

--- day_9.py
+++ day_9.py
@@ -5,7 +5,7 @@
 # \n use for going to new line
 
 days = "Mon Tue Wed Thu Fri Sat Sun "
-months = "\nJan\nFen\nMar\nApr\nJun\nJul\nAug"
+months = "\nJan\nFeb\nMar\nApr\nJun\nJul\nAug"
 
 print("here are the days : " ,days)
 print("here are the months : ", months)
@@ -18,3 +18,20 @@ print(
     we'll be able to type as much as we like.
     even 4 lines if we want, or 5 , or 6 .  
       """)
+
+#answer
+#here are the days :  Mon Tue Wed Thu Fri Sat Sun 
+#here are the months :  
+#Jan
+#Feb
+#Mar
+#Apr
+#Jun
+#Jul
+#Aug
+
+#there is something going on here .
+#with the three double_quotes.
+#we'll be able to type as much as we like.
+# even 4 lines if we want, or 5 , or 6 .  
+      
