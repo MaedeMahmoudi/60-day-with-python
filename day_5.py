a3f659000db9d7bd3eabc65e91f5e18df709f640
Maedeh Mahmoudi <maedeemahmoudi5@gmail.com>
2025-02-05 21:47:20 +0330

    strings

---
+++ day_5.py
@@ -0,0 +1,21 @@
+
+#day 5   stings
+
+my_name = 'zed A. shaw'
+my_age = 35 #not a lie :)
+my_height = 74  #inches
+my_weight = 180  #1bs
+my_eyes = 'blue'
+my_teeth = 'white'
+my_hair = 'brown'
+
+print ("let's talk about %s ." %my_name )
+print("he's %d inches tall." %my_height)
+print("he's %d pounds heavy."%my_weight )
+print("actually that's not too heavy.")
+print("he's got %s hair." %my_hair)
+print("his teeth are usually %s depending on the coffee." %my_teeth)
+
+#this line is tricky , try to get it exactly right
+
+print("if i add %d , %d , and %d i get %d ." %(my_age , my_height , my_weight , my_age + my_height + my_weight  )) 
